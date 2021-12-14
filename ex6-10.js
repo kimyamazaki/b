a925fa270599bff486c717ff785313ea7431afd6
kimyamazaki <zm20255@josai.ac.jp>
2021-12-14 01:57:25 +0000

    message

---
+++ ex6-10.js
@@ -0,0 +1,8 @@
+function imgchange1() {
+    document.getElementById('logo').src= 
+    './image/ 城サイ.jpg';}
+
+function imgchange2() {
+    document.getElementById('logo').src= 
+    './image/keiei_small.jpg';}
+
